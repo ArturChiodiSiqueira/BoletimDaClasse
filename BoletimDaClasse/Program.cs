4245a4a3c59df8cedbff89f49bce3f8106a4f115
artur <artur@DESKTOP-V0JU5PP>
2022-08-19 00:20:36 -0300

    save

--- BoletimDaClasse/Program.cs
+++ BoletimDaClasse/Program.cs
@@ -4,9 +4,28 @@ namespace BoletimDaClasse
 {
     internal class Program
     {
-
+        static int QuantidadeAlunos()
+        {
+            int quantidade;
+            Console.Write("Informe a quantidade de aluno: ");
+            quantidade = int.Parse(Console.ReadLine());
+            return quantidade;
+        }
         static void Main(string[] args)
         {
+
+
+
+
+
+            QuantidadeAlunos();
+
+
+
+
+
+
+
             int quantidade;
             Console.Write("Informe a quantidade de aluno: ");
             quantidade = int.Parse(Console.ReadLine());
@@ -17,10 +36,25 @@ namespace BoletimDaClasse
             {
                 Console.Write("Digite o nome do aluno #" + (alunos + 1) + ": ");
                 boletimClasse[alunos, 0] = Console.ReadLine();
+
                 Console.Write("informe a nota 1: ");
                 boletimClasse[alunos, 1] = Console.ReadLine();
+
+                while (Convert.ToDecimal(boletimClasse[alunos, 1]) < 0 || Convert.ToDecimal(boletimClasse[alunos, 1]) > 10)
+                {
+                    Console.WriteLine("nota invalida");
+                    Console.Write("informe a nota 1: ");
+                    boletimClasse[alunos, 1] = Console.ReadLine();
+                } 
                 Console.Write("informe a nota 2: ");
                 boletimClasse[alunos, 2] = Console.ReadLine();
+
+                while (Convert.ToDecimal(boletimClasse[alunos, 2]) < 0 || Convert.ToDecimal(boletimClasse[alunos, 2]) > 10)
+                {
+                    Console.WriteLine("nota invalida");
+                    Console.Write("informe a nota 2: ");
+                    boletimClasse[alunos, 2] = Console.ReadLine();
+                }
 
                 boletimClasse[alunos, 3] = ((Convert.ToDecimal(boletimClasse[alunos, 1]) + Convert.ToDecimal(boletimClasse[alunos, 2])) / 2).ToString();
             }
@@ -32,12 +66,13 @@ namespace BoletimDaClasse
 
                 if (Convert.ToDecimal(boletimClasse[alunos, 3]) >= 6)
                 {
-                    Console.WriteLine("Aluno Aprovado!");
+                    Console.WriteLine("\tAluno Aprovado!");
                 }
                 else 
                 {
-                    Console.WriteLine("Aluno Reprovado!");
+                    Console.WriteLine("\tAluno Reprovado!");
                 }
+                Console.ReadKey();
                 Console.WriteLine();
             }
         }
